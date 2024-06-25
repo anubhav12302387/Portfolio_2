7a4c79c42390e11a63fb8ace628be49570f8c8a7
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-06-25 12:18:31 +0530

    project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const NavbarContainer = styled.div``;
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      Navbar
+    </NavbarContainer>
+  )
+}
+
+export default Navbar
+
+
+
+//10.26 setup & first commit
