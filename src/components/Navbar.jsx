c4d71246ef0d0717f4c52341eca7483a01a36c26
Anubhav Jaiswal <anubhavjaiswal1803@gmail.com>
2024-06-26 19:03:03 +0530

    added Hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { Link as LinkR, } from "react-router-dom";
-import styled from 'styled-components'
-import {Bio} from "../data/constants";
+import React, { useState } from 'react';
+import { Link as LinkR } from "react-router-dom";
+import styled, { useTheme } from 'styled-components';
+import { Bio } from "../data/constants";
 import { MenuRounded } from "@mui/icons-material";
 
 const Nav = styled.div`
@@ -26,6 +26,7 @@ const NavbarContainer = styled.div`
   justify-content: space-between;
   font-size: 1rem;
 `;
+
 const NavLogo = styled(LinkR)`
   width: 80%;
   padding: 0.6px;
@@ -33,7 +34,6 @@ const NavLogo = styled(LinkR)`
   font-size: 18px;
   text-decoration: none;
   color: inherit;
-
 `;
 
 const NavItems = styled.ul`
@@ -93,46 +93,73 @@ const GithubButton = styled.a`
 
 const MobileIcon = styled.div`
   height: 100%;
-  dispaly: flex;
+  display: flex;
   align-items: center;
   color: ${({ theme }) => theme.text_primary};
   display: none;
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 768px) {
     display: block;
   }
 `;
 
+const MobileMenu = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  gap: 16px;
+  padding: 0 6px;
+  list-style: none;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => `${theme.card_light}99`};
+  position: absolute;
+  top: 80px;
+  right: 0;
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+  z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
+`;
 
 const Navbar = () => {
-  const[isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useTheme();
+
   return (
     <Nav>
       <NavbarContainer>
-    <NavLogo to="/">GeekForGeeks</NavLogo>
+        <NavLogo to="/">Anubhav Jaiswal</NavLogo>
 
-    <MobileIcon onClick={() => setIsOpen(!isOpen)}>
-      <MenuRounded style={{ color: "inherit" }} />
-    </MobileIcon>
+        <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+          <MenuRounded style={{ color: 'inherit' }} />
+        </MobileIcon>
 
-    <NavItems>
-      <NavLink href="#About">About</NavLink>
-      <NavLink href="#skills">Skills</NavLink>
-      <NavLink href="#Experience">Experience</NavLink>
-      <NavLink href="#Projects">Projects</NavLink>
-      <NavLink href="#Eduaction">Eduaction</NavLink>
-    </NavItems>
+        <NavItems>
+          <NavLink href="#About">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Projects">Projects</NavLink>
+          <NavLink href="#Education">Education</NavLink>
+        </NavItems>
 
-    <ButtonContainer>
-      <GithubButton href={Bio.github} target="_Blank">Github Profile</GithubButton>
-    </ButtonContainer>
-  </NavbarContainer></Nav>
-    
-  )
-}
+        <MobileMenu isOpen={isOpen}>
+          <NavLink onClick={() => setIsOpen(!isOpen)} href="#About">About</NavLink>
+          <NavLink onClick={() => setIsOpen(!isOpen)} href="#skills">Skills</NavLink>
+          <NavLink onClick={() => setIsOpen(!isOpen)} href="#Experience">Experience</NavLink>
+          <NavLink onClick={() => setIsOpen(!isOpen)} href="#Projects">Projects</NavLink>
+          <NavLink onClick={() => setIsOpen(!isOpen)} href="#Education">Education</NavLink>
+          <GithubButton href={Bio.github} target="_blank" style={{ background: theme.primary, color: theme.text_primary,}}>Github Profile</GithubButton>
+        </MobileMenu>
 
-export default Navbar
+        <ButtonContainer>
+          <GithubButton href={Bio.github} target="_blank">Github Profile</GithubButton>
+        </ButtonContainer>
+      </NavbarContainer>
+    </Nav>
+  );
+};
 
-
-
-//10.26 setup & first commit
-//25:00 navbar Hamburger menu
+export default Navbar;
